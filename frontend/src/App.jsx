73a579a9c397d10aad684867c53562e3bfbc01b2
Igli <iglibraho77@gmail.com>
2025-01-21 16:54:40 +0100

    Configured Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '.pages/Home'
+import Collections from './pages/Collections'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/collections' element={<Collections></Collections>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/product/:productId' element={<Product></Product>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/place-order' element={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,23 @@
+import logo from 'logo.jpg'
+import p_img1_1 from 'p_img1_1.jpg'
+import p_img1_2 from 'p_img1_2.jpg'
+
+export const assets = {
+    logo,
+    p_img1_1,
+    p_img1_2
+}
+
+export const products = [
+     {
+        _id: "1",
+        name: "Pantallonat pambuk",
+        description: "Pantallonat në masat 36/38/40, 97% pambuk 3% elastike",
+        image: [p_img1_1,p_img1_2],
+        category: "Women",
+        subcategory: "pants",
+        sizes: ["S", "M", "L"],
+        date: 12354665,
+        bestseller: true
+     }
+]
