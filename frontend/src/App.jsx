a2d0acd423b2fe53f86255e521466c132eda4dc2
Igli <iglibraho77@gmail.com>
2025-01-24 22:45:24 +0100

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from '.pages/Home'
+import Home from './pages/Home'
 import Collections from './pages/Collections'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -9,10 +9,12 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/collections' element={<Collections></Collections>}></Route>
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,21 @@
-import logo from 'logo.jpg'
-import p_img1_1 from 'p_img1_1.jpg'
-import p_img1_2 from 'p_img1_2.jpg'
+import logo from './logo.jpg'
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+import cart from './cart.png'
+import menu from './menu.png'
+import dropdown from './dropdown.png'
+import hero from './hero.jpg'
+import p_img1_1 from './p_img1_1.jpg'
+import p_img1_2 from './p_img1_2.jpg'
 
 export const assets = {
     logo,
+    search_icon,
+    profile_icon,
+    cart,
+    menu,
+    dropdown,
+    hero,
     p_img1_1,
     p_img1_2
 }
